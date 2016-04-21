d8c3091b9c9fcc92567f8521be4e8252ba4630d3
Garry Lachman <garrylachman_macbook@MacBook-Pro-2.local>
2016-04-21 03:09:55 +0300

    dist header

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,11 +12,19 @@ based on https://github.com/hannesgassert/countrynames
 
 module.exports = function(grunt) {
 
+	var header = "Copyright (C) 2016 Garry Lachman garry@lachman.co under GNU LGPL\n"+
+	"https://rev.proxies.online\n"+
+	"This library is free software; you can redistribute it and/or modify it under the terms of the\n"+
+	"GNU Lesser General Public License version 2.1, as published by the Free Software Foundation.\n"+
+	"This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;\n"+
+	"without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n"+
+	"See the GNU Lesser General Public License for more details.\n\n";
+
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		uglify: {
 			options: {
-				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+				banner: '/*! '+header+'<%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
  			},
 			build: {
 				src: 'src/<%= pkg.name %>.js',
